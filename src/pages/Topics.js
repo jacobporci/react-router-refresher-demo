34cb01406122ab79fe1eb0227b85eaca5c18f7cc
Jacob Porciuncula <jacobporci@gmail.com>
2021-05-23 18:48:02 +0800

    Simplified Topics page

---
+++ src/pages/Topics.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { LinkItem } from "../components/LinkItem";
+import { PageWrapper } from "../components/PageWrapper";
+import { useRenderCount } from "../useRenderCount";
+
+export const Topics = () => {
+  useRenderCount({ componentName: "<Topics />" });
+
+  return (
+    <PageWrapper>
+      <h1>Topics to Cover</h1>
+      <ul>
+        <LinkItem to="/intro">Intro</LinkItem>
+        <LinkItem to="/primary-components">Primary components</LinkItem>
+      </ul>
+    </PageWrapper>
+  );
+};
