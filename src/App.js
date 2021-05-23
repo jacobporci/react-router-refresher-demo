ea8b3194e7124361579814e2fab91bc5a099608a
Jacob Porciuncula <jacobporci@gmail.com>
2021-05-23 18:15:02 +0800

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div className="App">hello world</div>;
-}
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Intro } from "./pages/Intro";
+import { Start } from "./pages/Start";
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Route exact path="/" component={Intro} />
+      <Route path="/start" component={Start} />
+    </Router>
+  );
+};
 
 export default App;
