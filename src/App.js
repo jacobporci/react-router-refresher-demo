8c4ca41787c94586316c6da777cf6f736a25241f
Jacob Porciuncula <jacobporci@gmail.com>
2021-05-23 18:48:54 +0800

    Added routes to other pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
 import { Intro } from "./pages/Intro";
-import { Start } from "./pages/Start";
+import { PrimaryComponents } from "./pages/PrimaryComponents";
+import { Topics } from "./pages/Topics";
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Route exact path="/" component={Intro} />
-      <Route path="/start" component={Start} />
+      <Route exact path="/" component={Home} />
+      <Route path="/topics" component={Topics} />
+      <Route path="/intro" component={Intro} />
+      <Route path="/primary-components" component={PrimaryComponents} />
     </Router>
   );
 };
