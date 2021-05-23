72e16a8213ce3f713d6bbf7ad973a0677e861f24
Jacob Porciuncula <jacobporci@gmail.com>
2021-05-23 18:48:38 +0800

    Created Intro page

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Button } from "../components/Button";
 import { PageWrapper } from "../components/PageWrapper";
-import { useRenderCount } from "../useRenderCount";
 
 export const Intro = () => {
-  useRenderCount({ componentName: "<Intro />" });
-
   return (
     <PageWrapper>
-      <h1 style={{ fontSize: "60px" }}>React Router Refresher</h1>
-      <Link to="/start">
-        <h2>Start</h2>
-      </Link>
+      <h1 style={{ width: "1100px", lineHeight: 1.8, textAlign: "center" }}>
+        This guide’s purpose is to explain the mental model to have when using
+        React Router. We call it “Dynamic Routing”, which is quite different
+        from the “Static Routing” you’re probably more familiar with.
+      </h1>
+      <Button to="/primary-components">Next</Button>
     </PageWrapper>
   );
 };
