d8ef5929b14b5bba8a456f46c5e49027dd6a291c
Jacob Porciuncula <jacobporci@gmail.com>
2021-05-23 18:47:25 +0800

    Added routers and exact prop

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useRenderCount } from "../useRenderCount";
 
-const HeaderItem = ({ label, to }) => (
-  <NavLink to={to} activeStyle={{ color: "red" }} exact>
+const HeaderItem = ({ label, to, exact = false }) => (
+  <NavLink to={to} activeStyle={{ color: "red" }} exact={exact}>
     <h2 style={{ marginRight: "50px" }}>{label}</h2>
   </NavLink>
 );
@@ -23,8 +23,10 @@ export const Header = () => {
         borderBottom: "1px solid black",
       }}
     >
-      <HeaderItem to="/" label="Home" />
-      <HeaderItem to="/start" label="Table of Contents" />
+      <HeaderItem to="/" label="Home" exact />
+      <HeaderItem to="/topics" label="Topic to Cover" />
+      <HeaderItem to="/intro" label="Intro" />
+      <HeaderItem to="/primary-components" label="Primary Components" />
     </div>
   );
 };
