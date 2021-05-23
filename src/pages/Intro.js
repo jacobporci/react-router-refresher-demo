ba7a498d0c27d4df79f74e4a2df7be1863fcbd2b
Jacob Porciuncula <jacobporci@gmail.com>
2021-05-23 21:03:35 +0800

    Add useRenderCount hook to Intro

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Button } from "../components/Button";
 import { PageWrapper } from "../components/PageWrapper";
+import { useRenderCount } from "../useRenderCount";
 
 export const Intro = () => {
+  useRenderCount({ componentName: "<Intro />" });
+
   return (
     <PageWrapper>
       <h1 style={{ width: "70%", lineHeight: 1.8, textAlign: "center" }}>
