3961a59e251829d043343eace4a8eee1f2c64478
Jacob Porciuncula <jacobporci@gmail.com>
2021-05-29 14:17:02 +0800

    Add exact prop and fix style

--- src/components/NavLinkItem.js
+++ src/components/NavLinkItem.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export const NavLinkItem = ({ to, children }) => (
+export const NavLinkItem = ({ to, children, exact = false }) => (
   <NavLink
     to={to}
-    style={{ textDecoration: "none" }}
+    style={{ textDecoration: "none", margin: "0 10px" }}
     activeStyle={{ color: "red" }}
+    exact={exact}
   >
-    <li style={{ fontSize: "30px", marginBottom: "10px" }}>{children}</li>
+    <li style={{ fontSize: "20px", marginBottom: "10px" }}>{children}</li>
   </NavLink>
 );
